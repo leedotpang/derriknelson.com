291a18555f8cc829e422d5838c9d9fc237c5ee95
Mike <reingator@gmail.com>
2014-04-13 00:12:27 -0400

    Added CSS carousel to about page

--- about.php
+++ about.php
@@ -8,10 +8,26 @@
 	</head>
 	
 	<body>
-		<?php 
-		include("_header.php");
-		?>
-				
+	<?php 
+	include("_header.php");
+	?>
+
+		    <div id="slides-container">
+		    	<div class="slides"></div>
+		    	<div class="slides"></div>
+		    	<div class="slides"></div>	    	
+		    </div>
+		    
+		    <div class="slide-surface">
+		        <h1>WELCOME</h1>
+		        <h2>I'm Derrik Nelson</h2>
+		        <h3>Creating something incredible is my passion.<br>Composition is my niche.</h3>
+		        <button>Learn More</button>
+		        <div class="dot" id="dot-1"></div>
+		        <div class="dot" id="dot-2"></div>
+		        <div class="dot" id="dot-3"></div>
+		    </div>	
+
 	</body>
 
 </html>
