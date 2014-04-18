921cc0771e57bb3ea773549f5e114ce758750b9e
Mike <reingator@gmail.com>
2014-04-17 23:14:09 -0400

    Improved carousel; Added services section

--- _header.php
+++ _header.php
@@ -1,40 +1,37 @@
 <header>
 	<div class="container">
 		<object type="image/svg+xml" data="img/logo.svg" id="logo"><img id="logo" src="img/logo.png" alt="|DN|"/></object>
-				
-		<div id="logo-text">
-		    <p id="logo-name">DERRIK NELSON</p>
-		    <p id="logo-slogan">composition|counseling|christianity</p>
-		</div>
+		    
+		<div id="home"><a href="home.php">
+		    <div id="logo-text">
+		    	<p id="logo-name">DERRIK NELSON</p>
+		    	<p id="logo-slogan">composition|counseling|christianity</p>
+		    </div>
+		</div></a>   
 		    
 		<nav>
 		    <ul>
 		    	<?php	//Switch case to add the .active class to the current tab in the nav
 		    		switch($page) {
-			    		case("About"):
+			    		case("Portfolio"):
 			    			$class1 = "class='active'";
 			    			break;
-			    		case("Portfolio"):
+			    		case("Blog"):
 			    			$class2 = "class='active'";
 			    			break;
-			    		case("Blog"):
+			    		case("Contact"):
 			    			$class3 = "class='active'";
 			    			break;
-			    		case("Contact"):
-			    			$class4 = "class='active'";
-			    			break;
 			    		default:
+			    			$class1 = "class='null'";
 			    			$class2 = "class='null'";
 			    			$class3 = "class='null'";
-			    			$class1 = "class='null'";
-			    			$class4 = "class='null'";
 		    		}	
 		    		
 		    		echo '
-		    		<li ' . $class1 . '><a href="about.php">ABOUT</a></li>
-		    		<li ' . $class2 . '><a href="portfolio.php">PORTFOLIO</a></li>
-		    		<li ' . $class3 . '><a href="blog.php">BLOG</a></li>
-		     		<li ' . $class4 . '><a href="contact.php">CONTACT</a></li>
+		    		<li ' . $class1 . '><a href="portfolio.php">PORTFOLIO</a></li>
+		    		<li ' . $class2 . '><a href="blog.php">BLOG</a></li>
+		     		<li ' . $class3 . '><a href="contact.php">CONTACT</a></li>
 		     		<li id="search"><object type="image/svg+xml" data="img/search.svg"><img src="img/search.png" alt="search"/></object></li>';
 			 	?>
 		    </ul>
---
+++ home.php
@@ -0,0 +1,66 @@
+<!DOCTYPE html>
+<html lang="en-US">
+	<head>
+		<?php
+		$page = "Home";
+		include("_head.php");
+		?>
+	</head>
+	
+	<body>
+	<?php 
+	include("_header.php");
+	?>
+
+<!------Image Slider------>
+
+	<div id="slides-container">
+		<div class="slides"></div>
+		<div class="slides"></div>
+		<div class="slides"></div>
+	</div>
+	
+	<div class="slide-surface">
+	    <h1>WELCOME</h1>
+	    <h2>I'm Derrik Nelson</h2>
+	    <h3>"Creating something incredible is my passion.<br>Composition is my niche."</h3>
+	    <button>Learn More</button>
+	    <div class="dot" id="dot-1"></div>
+	    <div class="dot" id="dot-2"></div>
+	    <div class="dot" id="dot-3"></div>
+	</div>	
+
+<!------Services Blocks------>
+	
+	<div class="container" id="services-wrap">
+		<div class="services" id="pen">
+			<div><img src="img/pen.png"/></div>
+			<h2>MODERN DESIGN</h2>
+			<p>Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt.</p>
+			<button class="read-more">READ MORE</button>
+		</div>
+		
+		<div class="services" id="code">
+			<div><img src="img/code.png"/></div>
+			<h2>DOER</h2>
+			<p>Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt.</p>
+			<button class="read-more">READ MORE</button>
+		</div>
+		
+		<div class="services" id="preserver">
+			<div><img src="img/preserver.png"/></div>
+			<h2>SAVER</h2>
+			<p>Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt.</p>
+			<button class="read-more">READ MORE</button>
+		</div>
+		
+		<div class="services" id="bulb">
+			<div><img src="img/bulb.png"/></div>
+			<h2>THINKER</h2>
+			<p>Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt.</p>
+			<button class="read-more">READ MORE</button>
+		</div>
+	</div>
+
+</html>
+   
