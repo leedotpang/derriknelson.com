72a0cd262b0c354e918f7d5bdca35bbc2f063df8
Mike <reingator@gmail.com>
2014-05-25 23:35:59 -0400

    combined all js

--- _head.php
+++ _head.php
@@ -1,9 +1,8 @@
 <meta charset="utf-8">
 <link rel="icon" type="image/png" href="favicon.png"/>
-<link rel="apple-touch-icon" type="image/png" sizes="152x152" href="img/touch-icon.png">
+<link rel="apple-touch-icon" type="image/png" sizes="152x152" href="img/apple-touch-icon.png">
 
 <link rel="stylesheet" href="css/style.css">
 <link href="http://fonts.googleapis.com/css?family=Open+Sans:300,400,700" rel="stylesheet" type="text/css">
 
-<script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
-<script src="js/glide.min.js"></script>
+<script src="js/glide-jq-min.js"></script>
