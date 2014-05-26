dd6cf784c5b9904a3964e6510cdbe809e693c90e
Mike <reingator@gmail.com>
2014-05-25 23:37:00 -0400

    404 page

---
+++ 404.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en-US">
+	<head>
+		<title>404 Error – Derrik Nelson</title>
+		<?php
+		$page = "";
+		include("_head.php");
+		?>
+	</head>
+	
+	<body>
+		<?php 
+		include("_header.php"); 
+		?>
+		<div id="fourohfour">
+			<span>404</span>
+    		<p>You must be lost!<br/>Try a search for what you're looking for:</p>
+    		<li id="search"><img src="img/search.svg" alt="search"/></li>
+		</div>
+	</body>
+	
+	<footer>
+		<?php 
+			include("_footer.php");
+		?>
+	</footer>
+</html>
